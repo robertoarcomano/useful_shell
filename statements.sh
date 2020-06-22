48699859c17bfa025b90df577315b37439fecd3b
Roberto Arcomano <bertolinux@gmail.com>
2020-06-22 23:46:22 +0100

    Added new checks

--- statements.sh
+++ statements.sh
@@ -2,12 +2,18 @@
 
 if [ -z "$1" ]; then
   echo "Please specify <param1>"
-  exit
+  exit 1
 fi
 
 if [ ! -n "$2" ]; then
   echo "Please specify <param2>"
-  exit
+  exit 2
+fi
+
+if [[ (-n "$1") && (-n "$2") && (-z "$3" || -z "$4") ]]; then
+  echo "Please specify <param3> and <param4>"
+  exit 34
 fi
 
 echo $0 $1 $2
+exit 0
